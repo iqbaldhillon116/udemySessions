44a8535c8cfcfbee2ec74d9a575cc8f163f76741
iqbal <dhillon.iqbal.116@gmail.com>
2021-05-22 09:42:22 +0530

    sessions section 24 . worked in homecontroller

---
+++ laravel/app/Http/Controllers/HomeController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(Request $request)
+    {
+
+        //section 24 how to set sessions
+
+        $request->session()->put(['user'=>'iqbal']);
+
+        // echo $request->session()->get('user');
+
+        //global sessions
+
+        // session(['user2'=>'iqbal dhillon']);
+
+        // return session('user2');
+
+        //global sessions ends
+
+        //destroying sessions
+
+        // $request->session()->forget('user');
+
+        // print_r( $request->session()->all());
+
+        //destroying sessions ends
+
+        //destroying all sesion at once
+
+        // $request->session()->flush();
+
+        // print_r(  $request->session()->all());
+
+
+
+        return view('home');
+    }
+}
